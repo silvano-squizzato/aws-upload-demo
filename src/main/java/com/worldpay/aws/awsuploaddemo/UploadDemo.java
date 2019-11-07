013e4a2e5356ff9dbf2f3d041829a12dde5b77fc
Silvano Squizzato <silvano.squizzato@worldpay.com>
2019-11-07 15:24:07 +0000

    Upload to a secure bucket with assume role, allowing open download access

--- src/main/java/com/worldpay/aws/awsuploaddemo/UploadDemo.java
+++ src/main/java/com/worldpay/aws/awsuploaddemo/UploadDemo.java
@@ -2,7 +2,7 @@ package com.worldpay.aws.awsuploaddemo;
 
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.SdkClientException;
-import com.amazonaws.auth.STSAssumeRoleSessionCredentialsProvider;
+import com.amazonaws.auth.*;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
@@ -10,35 +10,47 @@ import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 
 import java.io.File;
-import java.io.IOException;
 
 public class UploadDemo {
 
-    public static void main(String[] args) throws IOException {
-        String bucketName = "cr-demo-upload";
-        String fileObjKeyName = "cr-demo-file-9.txt";
-        String fileName = UploadDemo.class.getClassLoader().getResource("cr-demo-file-1.txt").getPath();
+    private static AWSCredentialsProvider getAnonymousCredentialsProvider() {
+        return new AWSStaticCredentialsProvider(new AnonymousAWSCredentials());
+    }
+
+    private static AWSCredentialsProvider getAssumeRoleCredentialsProvider() {
+        return new STSAssumeRoleSessionCredentialsProvider.Builder(
+                "arn:aws:iam::388570974987:role/wp_power_role",
+                "demo")
+                .build();
+    }
+
+    private static AmazonS3 getS3Client() {
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(Regions.EU_WEST_1)
+                .withCredentials(getAssumeRoleCredentialsProvider())
+                .build();
+    }
+
+    public static void main(String[] args) {
+        String bucketName = "secure-tms-upload";
+        String fileObjKeyName = "demo.zip";
+        String filePath = UploadDemo.class.getClassLoader().getResource("demo.zip").getPath();
 
         try {
-            STSAssumeRoleSessionCredentialsProvider credentials = new STSAssumeRoleSessionCredentialsProvider.Builder(
-                    "arn:aws:iam::388570974987:role/wp_power_role",
-                    "test")
-                    .build();
+            AmazonS3 s3Client = getS3Client();
 
-            // Provide temporary security credentials so that the Amazon S3 client
-            // can send authenticated requests to Amazon S3.
-            AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
-                    .withRegion(Regions.EU_WEST_1)
-                    .withCredentials(credentials)
-                    .build();
+            System.out.println("S3 client created");
 
             // Upload a file as a new object with ContentType and title specified.
-            PutObjectRequest request = new PutObjectRequest(bucketName, fileObjKeyName, new File(fileName));
+            PutObjectRequest request = new PutObjectRequest(bucketName, fileObjKeyName, new File(filePath));
             ObjectMetadata metadata = new ObjectMetadata();
-            metadata.setContentType("plain/text");
-            metadata.addUserMetadata("x-amz-meta-title", "someTitle");
+            metadata.setContentType("application/zip");
+            metadata.addUserMetadata("x-amz-meta-title", "demo");
             request.setMetadata(metadata);
             s3Client.putObject(request);
+
+            System.out.println("Upload successful, file url: " + s3Client.getUrl(bucketName, fileObjKeyName));
+
         } catch (AmazonServiceException e) {
             // The call was transmitted successfully, but Amazon S3 couldn't process
             // it, so it returned an error response.
